a5f467e72e2b657c2b0165e23b3291844993fb70
nsaspook <fred@sma2.rain.com>
2018-10-23 19:01:54 -0700

    update code

--- pfb.c
+++ pfb.c
@@ -2,12 +2,12 @@
 
 // resolver angle data from controller parser
 
-uint16_t get_pfb(char * buf)
+uint16_t get_pfb(const char * buf)
 {
 	float offset, mphase, offset_whole;
 	char *token, pfb_ascii[BT_RX_PKT_SZ + 2], s[2] = " ";
 
-	strcpy(pfb_ascii, buf);
+	strcpy(pfb_ascii, buf); // make a local copy of the data
 	token = strtok(pfb_ascii, s); // start token search
 	token = strtok(NULL, s); // look for the second number
 
@@ -15,7 +15,7 @@ uint16_t get_pfb(char * buf)
 		mphase = atof(token);
 		offset = ((MOTOR_POLES / MOTOR_PAIRS) * mphase) / 360.0;
 		offset_whole = trunc(offset); // get the whole part with no rounding
-		offset = (offset - offset_whole)*360.0; // extract fractional part for angle offset
+		offset = (offset - offset_whole)*360.0; // extract fractional part for angle offset calc
 		/* need to round and convert data to integer */
 		return(uint16_t) roundf(offset);
 	} else
